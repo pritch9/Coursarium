065ed97d072f8325aee2fc24f39c5db3f4c23fb4
andrewqu <acqu@buffalo.edu>
2019-03-06 16:01:07 -0500

    Update to Authentication

--- Service/UserService/Authentication.js
+++ Service/UserService/Authentication.js
@@ -25,24 +25,29 @@ exports.register = function(req, res) {
   // school is a number > 0
   const numberRegExp = new RegExp('/\d+/g');
   if(!school.match(numberRegExp)) {
-    res.send("School validation failed");
+    return res.status(200).json({error: "School Validation Failed"});
+    // res.send("School validation failed");
   }
     // email is a valid email address format max length is 120
   var buf = "@buffalo.edu";
   if(!email.includes(buf)) {
-    res.send("Email validation failed");
+    return res.status(200).json({error: "Email Validation Failed"});
+  //  res.send("Email validation failed");
   }
     // password is a string, we can figure out strength. Check password length, must have numbers, symbols, and a capital.
     // Pattern: (?=^.{8,100}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;'?/&gt;.&lt;,])(?!.*\s).*$
     //Description: Requires 1 lowercase, 1 uppercase, 1 digit, and 1 special character. Minimum length: 8.
   const passRegExp = new RegExp('(?=^.{8,100}$)(?=.*\\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&amp;*()_+}{&quot;:;\'?/&gt;.&lt;,])(?!.*\\s).*$');
   if(!password.match(passRegExp)) {
-    res.send("Password validation failed");
+    return res.status(200).json({error: "Password Validation Failed"});
+  //  res.send("Password validation failed");
   }
     // firstname, last name, full name just letter characters /([A-Za-z]|\ )*/g max length for first and last is 51
   if(full_name.length > 51) {
-    res.send("Name is too long.");
+    return res.status(200).json({error: "Name Validation Failed"});
+  //  res.send("Name is too long.");
   } else {
+    //Pass info onto database
     res.send("All tests passed.");
   }
 
